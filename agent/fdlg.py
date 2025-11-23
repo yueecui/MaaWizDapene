d07bfc1bff8dfd7a8a69b3bb5e6e04cc609764cc
Yuee <yuee@qq.com>
2025-11-23 18:36:38 +0800

    添加无法找到路线的提示判断

--- agent/fdlg.py
+++ agent/fdlg.py
@@ -62,6 +62,7 @@ class FDLGInitData(CustomAction):
                 # 循环移动
                 "点击自动移动": {"next": [f"{prefix}：【循环】移动中"]},
                 "点击自动移动（识图）": {"next": [f"{prefix}：【循环】移动中"]},
+                "无法找到路线": {"next": [f"{prefix}：执行入口"]},
                 "战斗：回到行走画面": {"next": [f"{prefix}：【循环】移动中"]},
             }
         )
